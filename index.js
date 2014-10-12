ad9717143f4b272f0065e9dd1f1121619bbbf04b
Brian Mullan <bmullan91@gmail.com>
2014-10-12 14:32:42 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var EventEmitter = require('events').EventEmitter;
+
+function EventListener(emitter, event) {
+  if (!(this instanceof EventListener)){
+    return new EventListener(emitter, event);
+  }
+
+  if(!(emitter instanceof EventEmitter)) {
+    throw new Error('emitter must be an instance of EventEmitter');
+  }
+
+  if(!event || typeof event !== 'string') {
+    throw new Error('invalid event');
+  }
+
+  this.emitter = emitter;
+  this.event = event;
+  this.cb = null;
+};
+
+EventListener.prototype.on = function(cb) {
+  this.cb = cb;
+  this.emitter.on(this.event, cb);
+};
+
+EventListener.prototype.once = function(cb) {
+  this.cb = cb;
+  this.emitter.once(this.event, cb);
+};
+
+EventListener.prototype.off = function() {
+  this.emitter.removeListener(this.event, this.cb);
+};
+
+module.exports = EventListener;
+module.exports.EventListener = EventListener;
